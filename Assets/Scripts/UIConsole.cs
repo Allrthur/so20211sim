128d4f0aeed218c04819ae14edea06e04a65a12d
Allrthur <arthurbittencourt2@gmail.com>
2021-07-27 16:08:10 -0300

    Console finalmente funcionando
    Botão direiro pra adicionar uma linha no console. Precisa de uma revisão, pois muitas linhas causam slowdown

--- Assets/Scripts/UIConsole.cs
+++ Assets/Scripts/UIConsole.cs
@@ -5,27 +5,39 @@ using UnityEngine.UI;
 
 public class UIConsole : MonoBehaviour
 {
-    public Text ctext;
+    public GameObject container;
+    public GameObject textline;
     public ScrollRect sr;
     private int z = 0;
-    public float f = 50;
     
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1")) 
+        if (Input.GetButtonDown("Fire2")) CPrint();
+
+    }
+
+    void CPrint()
+    {
+        z = z + 1;
+
+        GameObject txt = Instantiate(textline, container.transform);
+        txt.GetComponent<Text>().text = " - Linha " + z.ToString();
+
+
+        //autoscroll down
+        if (sr.verticalNormalizedPosition <= 0.1)
         {
-            z = z + 1;
-            ctext.text = ctext.text + "\n - Linha " + z.ToString();
-            sr.velocity = new Vector2(0f, 60f);
+            sr.verticalNormalizedPosition = 0;
+            sr.velocity = new Vector2(0f, 100f);
         }
- 
+        else sr.velocity = new Vector2(0f, -10f);
+
     }
 
 
